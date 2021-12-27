1575b7dea697a338d6155b8db0168b54d8c882c4
Nicolas Saby <nicolas.saby@inrae.fr>
2021-12-27 16:33:34 +0100

    add data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,14 @@
-## code to prepare `DATASET` dataset goes here
+## code to prepare `parc` dataset goes here
+library(sf)
+library(raster)
+r <- raster("../data/mnt_par_adj/")
+s <- terrain(r,opt = c("slope", "aspect", "TPI", "TRI") )
+scrop <- crop(s,parc)
+scrop <- mask(scrop,parc)
 
-usethis::use_data(DATASET, overwrite = TRUE)
+rcrop <- crop(r,parc)
+rcrop <- crop(rcrop,parc)
+
+usethis::use_data(parc, overwrite = TRUE)
+usethis::use_data(scrop, overwrite = TRUE)
+usethis::use_data(rcrop, overwrite = TRUE)
